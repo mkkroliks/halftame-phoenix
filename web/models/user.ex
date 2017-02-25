6144e22f12842be0c5692d84db2ca91e719342ce
Maciej Krolikowski <mkkrolik@gmail.com>
2017-02-25 15:56:43 +0100

    working login with facebook

--- web/models/user.ex
+++ web/models/user.ex
@@ -2,9 +2,13 @@ defmodule Halftame.User do
   require IEx
   use Halftame.Web, :model
 
+  alias Halftame.User
+
   schema "users" do
-    field :name, :string
-    field :token, :string
+    field :first_name, :string
+    field :fb_token, :string
+    field :photo, :string
+    field :email, :string
 
     timestamps()
   end
@@ -12,10 +16,11 @@ defmodule Halftame.User do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
+
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :token])
-    |> validate_required([:name, :token])
+    |> cast(params, [:first_name, :fb_token, :photo, :email])
+    |> validate_required([:first_name, :fb_token, :photo, :email])
   end
 
   def login_by_facebook_token(conn, token, opts) do
@@ -24,17 +29,38 @@ defmodule Halftame.User do
     {:json, map} = facebook_user_params(token)
     longlife_token = Map.get(map, "access_token")
 
-    {:json, params} = Facebook.me("email, first_name, photo", longlife_token)
-    params = Map.put(params, :fb_access_token, longlife_token)
+    # IEx.pry
+
+    {:json, string_key_map} = Facebook.me("email, first_name, picture", longlife_token)
+
+    atom_map = for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), val}
+    # IEx.pry
+
+    %{"data" => picture_dict} = atom_map[:picture]
+    photo_url = Map.get(picture_dict, "url")
+
+    params = atom_map
+    |> Map.delete(:picture)
+    |> Map.put(:photo, photo_url)
+    |> Map.put(:fb_token, longlife_token)
+
+    IEx.pry
 
     case params do
-      %{email: email, first_name: first_name, photo: photo, fb_access_token: longlife_token} ->
-          if user = repo.get_by(Halftame.User, email: email) do
-            {:ok, user}
-          else
-            changeset = Halftame.User.changeset(%Halftame.User{}, params)
+      %{email: email, first_name: first_name, photo: photo, fb_token: longlife_token} ->
+        IEx.pry
+          if user = repo.get_by(User, email: email) do
+            changeset = User.changeset(user, params)
 
-            case Repo.insert(changeset) do
+            case repo.update(changeset) do
+              {:ok, user} ->
+                {:ok, user}
+              {:error, changest} ->
+                {:ok, user}
+            end
+          else
+            changeset = User.changeset(%User{}, params)
+            case repo.insert(changeset) do
             {:ok, user} ->
               {:ok, user}
             {:error, changest} ->
