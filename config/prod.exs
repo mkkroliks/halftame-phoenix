6897309c5245b1ca19af333e12210418d526b1d0
Maciej Krolikowski <mkkrolik@gmail.com>
2017-03-17 16:47:27 +0100

    fix in prod config file

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :halftame, Halftame.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :halftame, Halftame.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
