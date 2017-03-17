0904521ea236262f44a0e9012a353e7a0fa91ce0
Maciej Krolikowski <mkkrolik@gmail.com>
2017-03-17 15:34:52 +0100

    added parameter to autocomplete method in goolge model

--- web/models/google.ex
+++ web/models/google.ex
@@ -1,7 +1,7 @@
 defmodule Halftame.Google do
 
   def autocomplete(input) do
-    %{"predictions" => cities} = Google.Apis.Places.autocomplete(input)
+    %{"predictions" => cities} = Google.Apis.Places.autocomplete(input, [language: "pl", components: "country:pl"])
     Enum.map(cities, fn(x) ->  %{name: Map.get(x, "description"),
                                   id: Map.get(x, "place_id")
                                 }
